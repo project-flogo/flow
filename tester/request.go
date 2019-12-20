9f07199b86dd54c1b37f7f9be01466cd65494ffa
Frank Martinez <fmartinez@tibco.com>
2019-12-20 14:07:30 -0500

    extract flow uri in restart request

--- tester/request.go
+++ tester/request.go
@@ -93,8 +93,14 @@ func (rp *RequestProcessor) RestartFlow(restartRequest *RestartRequest) (results
 
 	logger.Debugf("Tester restarting flow")
 
+	//todo share action, for now add flowUri to settings
+	settings := map[string]interface{}{"flowURI":restartRequest.InitialState.FlowURI()}
+
 	factory := action.GetFactory(RefFlow)
-	act, _ := factory.New(&action.Config{})
+	act, err := factory.New(&action.Config{Settings:settings})
+	if err != nil {
+		return nil, err
+	}
 
 	inputs := make(map[string]interface{}, len(restartRequest.Data)+1)
 
