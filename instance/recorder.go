9ca80df1d72962f59b12765f302b98b7b86a0d1c
Tracy Li <tibcotracy@gmail.com>
2021-10-12 09:25:33 -0500

    Fix missing step recorder when subflow failed (#163)
    * Fix missed step recording when call subflow failed
    
    * fix typo

---
+++ instance/recorder.go
@@ -0,0 +1,38 @@
+package instance
+
+import (
+	"github.com/project-flogo/flow/state"
+	"time"
+)
+
+type stateInstanceRecorder struct {
+	mod              state.RecordingMode
+	externalRecorder state.Recorder
+}
+
+func NewStateInstanceRecorder(recorder state.Recorder, mod state.RecordingMode) *stateInstanceRecorder {
+	return &stateInstanceRecorder{
+		mod:              mod,
+		externalRecorder: recorder,
+	}
+}
+
+func (inst *IndependentInstance) RecordState(strtTime time.Time) error {
+	if state.RecordSnapshot(inst.instRecorder.mod) {
+		err := inst.instRecorder.externalRecorder.RecordSnapshot(inst.Snapshot())
+		if err != nil {
+			inst.logger.Warnf("unable to record snapshot: %v", err)
+		}
+	}
+
+	if state.RecordSteps(inst.instRecorder.mod) {
+		currStep := inst.CurrentStep(true)
+		currStep.StartTime = strtTime
+		currStep.EndTime = time.Now().UTC()
+		err := inst.instRecorder.externalRecorder.RecordStep(currStep)
+		if err != nil {
+			inst.logger.Warnf("unable to record step: %v", err)
+		}
+	}
+	return nil
+}
