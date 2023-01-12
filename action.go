0174fd175da449934e89c76cdd9ee5c13269ef5a
Ashutosh Bhide <abhide@tibco.com>
2023-01-12 12:23:19 +0530

    Handle flow output case

--- action.go
+++ action.go
@@ -411,6 +411,7 @@ func (fa *FlowAction) applyAssertionInterceptor(inst *instance.IndependentInstan
 	if inst.GetInterceptor() != nil {
 		interceptor := inst.GetInterceptor().GetTaskInterceptor(inst.Instance.Name() + "-_flowOutput")
 		if interceptor != nil {
+			interceptor.Result = flowsupport.Pass
 			ef := expression.NewFactory(definition.GetDataResolver())
 			for id, assertion := range interceptor.Assertions {
 				if assertion.Expression == "" {
