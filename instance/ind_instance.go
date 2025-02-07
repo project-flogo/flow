e075c6a8e5ebd11ca036f7a378c031394d23d88f
Ashutosh Bhide <abhide@tibco.com>
2025-02-07 16:10:46 +0530

    Added optimization code when execution history is not enabled

--- instance/ind_instance.go
+++ instance/ind_instance.go
@@ -3,6 +3,7 @@ package instance
 import (
 	"errors"
 	"fmt"
+	"os"
 	"strconv"
 	"time"
 
@@ -24,8 +25,8 @@ type IndependentInstance struct {
 	workItemQueue *support.SyncQueue //todo: change to faster non-threadsafe queue
 	wiCounter     int
 
-	//trackingChanges bool
-	changeTracker ChangeTracker
+	trackingChanges bool
+	changeTracker   ChangeTracker
 
 	flowModel   *model.FlowModel
 	patch       *flowsupport.Patch
@@ -67,6 +68,9 @@ func NewIndependentInstance(instanceID string, flowURI string, flow *definition.
 	inst.logger = logger
 
 	inst.status = model.FlowStatusNotStarted
+	if os.Getenv("FLOGO_FLOW_SM_ENDPOINT") != "" {
+		inst.trackingChanges = true
+	}
 	inst.changeTracker = NewInstanceChangeTracker(inst.id, 0)
 	inst.changeTracker.FlowCreated(inst)
 
